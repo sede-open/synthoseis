a9677b50624c1d5cdbc5845cf8c5c7baf2824d4b
spaceswimmer <spaceswimmer69@gmail.com>
2026-03-29 21:06:21 +0300

    main changed rpm

--- rockphysics/rpm_tagilsk.py
+++ rockphysics/rpm_tagilsk.py
@@ -5,10 +5,10 @@ import os
 # Add the parent directory to the system path
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
 sys.path.insert(0, project_root)
-import time
-from src.util.filehandler import load_gp_grid_params
-from src.util.gaussian_processes import sample_gp_grid
+from src.util.filehandler import load_chebyshev_approximation
+from src.util.gaussian_processes import fast_chebyshev_evaluate
 
+#3800 max depth
 class RPMTagilsk():
     def __init__(self, cfg, *args, **kwargs):
         self.cfg = cfg
@@ -43,160 +43,138 @@ class RPMTagilsk():
     
     @staticmethod
     def calc_shale_rho(z):
-        return 7.7e-12 * z**3 + -8.8e-08 * z**2 + 0.0004 * z + 1.957
+        return 7.7e-12 * z**3 + -8.8e-08 * z**2 + 0.0004 * z + 1.887
 
     @staticmethod
     def calc_shale_vp(z):
-        return -0.00013 * z**2 + 1.13 * z + 1580
+        return -0.00013 * z**2 + 1.13 * z + 1620
 
     @staticmethod
     def calc_shale_vs(z):
-        return -0.0001 * z**2 + 0.96 * z + 279
+        return -0.0001 * z**2 + 0.96 * z + 579
 
     @staticmethod
     def calc_brine_sand_rho(z):
-        return -7.8e-09 * z**2 + 0.00012 * z + 2.021
+        return -7.8e-09 * z**2 + 0.00012 * z + 1.985
 
     @staticmethod
     def calc_brine_sand_vp(z):
-        return -1.34e-05 * z**2 + 0.49 * z + 2117
+        return -1.34e-05 * z**2 + 0.49 * z + 2197
 
     @staticmethod
     def calc_brine_sand_vs(z):
-        return -1.0785e-05 * z**2 + 0.391 * z + 1007
+        return -1.0785e-05 * z**2 + 0.391 * z + 1157
 
     @staticmethod
     def calc_oil_sand_rho(z):
-        return -9.23e-09 * z**2 + 0.00014 * z + 1.916
+        return -9.23e-09 * z**2 + 0.00014 * z + 1.976
 
     @staticmethod
     def calc_oil_sand_vp(z):
-        return -8.876e-06 * z**2 + 0.505 * z + 1998
+        return -8.876e-06 * z**2 + 0.505 * z + 1838
 
     @staticmethod
     def calc_oil_sand_vs(z):
-        return -1.126e-05 * z**2 + 0.391 * z + 636
+        return -1.126e-05 * z**2 + 0.391 * z + 656
 
     @staticmethod
     def calc_gas_sand_rho(z):
-        return -1.818e-08 * z**2 + 0.000247 * z + 1.912
+        return -1.818e-08 * z**2 + 0.000247 * z + 1.900
 
     @staticmethod
     def calc_gas_sand_vp(z):
-        return -3.216e-06 * z**2 + 0.4796 * z + 1996
+        return -3.216e-06 * z**2 + 0.4796 * z + 1966
 
     @staticmethod
     def calc_gas_sand_vs(z):
         return -1.0687e-05 * z**2 + 0.3662 * z + 1135
-    
-    def _load_shale_grid(self):
-        if self._shale_grid is None:
-            self._shale_grid = load_gp_grid_params('../data/grids/shale_pl_vp_vs.npz')
-        return self._shale_grid
-
-    def _load_brine_grid(self):
-        if self._brine_grid is None:
-            self._brine_grid = load_gp_grid_params('../data/grids/brine_pl_vp_vs.npz')
-        return self._brine_grid
-
-    def _load_oil_grid(self):
-        if self._oil_grid is None:
-            self._oil_grid = load_gp_grid_params('../data/grids/oil_pl_vp_vs.npz')
-        return self._oil_grid
-
-    def _load_gas_grid(self):
-        if self._gas_grid is None:
-            self._gas_grid = load_gp_grid_params('../data/grids/gas_pl_vp.npz')
-        return self._gas_grid
 
     def calc_shale_properties(self, z_rho, z_vp, z_vs):
-        """
-        Sample shale properties at specified depths.
+
+        cheb_params = load_chebyshev_approximation('../data/grids/chebyshev/shale_pl_vp_vs.json')
+        mean_rho, _ = fast_chebyshev_evaluate(cheb_params, z_rho)
+        mean_rho = mean_rho[:, 0]
+        mask_rho = (z_rho >= 1250) & (z_rho <= 3800)
+        rho = self.calc_shale_rho(z_rho)
+        rho[mask_rho] = mean_rho[mask_rho]
+
+        mean_vp, _ = fast_chebyshev_evaluate(cheb_params, z_vp)
+        mean_vp = mean_vp[:, 1]
+        mask_vp = (z_vp >= 1250) & (z_vp <= 3800)
+        vp = self.calc_shale_vp(z_vp)
+        vp[mask_vp] = mean_vp[mask_vp]
+
+        mean_vs, _ = fast_chebyshev_evaluate(cheb_params, z_vs)
+        mean_vs = mean_vs[:, 2]
+        mask_vs = (z_vs >= 1250) & (z_vs <= 3800)
+        vs = self.calc_shale_vs(z_vs)
+        vs[mask_vs] = mean_vs[mask_vs]
         
-        Args:
-            z_rho: Depth array for density sampling
-            z_vp: Depth array for Vp sampling
-            z_vs: Depth array for Vs sampling
-            
-        Returns:
-            RockProperties object with sampled values
-        """
-        grid = self._load_shale_grid()
-        
-        # Sample GP with separate z arrays and linear extrapolation
-        samples = sample_gp_grid(
-            grid,
-            z_new=[z_rho, z_vp, z_vs],
-            n_samples=1,
-            linear_extrap_funcs=[
-                self.calc_shale_rho,
-                self.calc_shale_vp,
-                self.calc_shale_vs
-            ]
-        )
-        
-        rho_gp = samples[0, :len(z_rho), 0]
-        vp_gp = samples[0, :len(z_vp), 1]
-        vs_gp = samples[0, :len(z_vs), 2]
-        
-        return RockProperties(rho_gp, vp_gp, vs_gp)
+        return RockProperties(rho, vp, vs)
 
     def calc_brine_sand_properties(self, z_rho, z_vp, z_vs):
-        grid = self._load_brine_grid()
+        cheb_params = load_chebyshev_approximation('../data/grids/chebyshev/brine_pl_vp_vs.json')
         
-        samples = sample_gp_grid(
-            grid,
-            z_new=[z_rho, z_vp, z_vs],
-            n_samples=1,
-            linear_extrap_funcs=[
-                self.calc_brine_sand_rho,
-                self.calc_brine_sand_vp,
-                self.calc_brine_sand_vs
-            ]
-        )
+        mean_rho, _ = fast_chebyshev_evaluate(cheb_params, z_rho)
+        mean_rho = mean_rho[:, 0]
+        mask_rho = (z_rho >= 1250) & (z_rho <= 3800)
+        rho = self.calc_brine_sand_rho(z_rho)
+        rho[mask_rho] = mean_rho[mask_rho]
+
+        mean_vp, _ = fast_chebyshev_evaluate(cheb_params, z_vp)
+        mean_vp = mean_vp[:, 1]
+        mask_vp = (z_vp >= 1250) & (z_vp <= 3800)
+        vp = self.calc_brine_sand_vp(z_vp)
+        vp[mask_vp] = mean_vp[mask_vp]
+
+        mean_vs, _ = fast_chebyshev_evaluate(cheb_params, z_vs)
+        mean_vs = mean_vs[:, 2]
+        mask_vs = (z_vs >= 1250) & (z_vs <= 3800)
+        vs = self.calc_brine_sand_vs(z_vs)
+        vs[mask_vs] = mean_vs[mask_vs]
         
-        rho_gp = samples[0, :len(z_rho), 0]
-        vp_gp = samples[0, :len(z_vp), 1]
-        vs_gp = samples[0, :len(z_vs), 2]
-        
-        return RockProperties(rho_gp, vp_gp, vs_gp)
+        return RockProperties(rho, vp, vs)
 
     def calc_oil_sand_properties(self, z_rho, z_vp, z_vs):
-        grid = self._load_oil_grid()
+        cheb_params = load_chebyshev_approximation('../data/grids/chebyshev/oil_pl_vp_vs.json')
         
-        samples = sample_gp_grid(
-            grid,
-            z_new=[z_rho, z_vp, z_vs],
-            n_samples=1,
-            linear_extrap_funcs=[
-                self.calc_oil_sand_rho,
-                self.calc_oil_sand_vp,
-                self.calc_oil_sand_vs
-            ]
-        )
+        mean_rho, _ = fast_chebyshev_evaluate(cheb_params, z_rho)
+        mean_rho = mean_rho[:, 0]
+        mask_rho = (z_rho >= 1250) & (z_rho <= 3800)
+        rho = self.calc_oil_sand_rho(z_rho)
+        rho[mask_rho] = mean_rho[mask_rho]
+
+        mean_vp, _ = fast_chebyshev_evaluate(cheb_params, z_vp)
+        mean_vp = mean_vp[:, 1]
+        mask_vp = (z_vp >= 1250) & (z_vp <= 3800)
+        vp = self.calc_oil_sand_vp(z_vp)
+        vp[mask_vp] = mean_vp[mask_vp]
+
+        mean_vs, _ = fast_chebyshev_evaluate(cheb_params, z_vs)
+        mean_vs = mean_vs[:, 2]
+        mask_vs = (z_vs >= 1250) & (z_vs <= 3800)
+        vs = self.calc_oil_sand_vs(z_vs)
+        vs[mask_vs] = mean_vs[mask_vs]
         
-        rho_gp = samples[0, :len(z_rho), 0]
-        vp_gp = samples[0, :len(z_vp), 1]
-        vs_gp = samples[0, :len(z_vs), 2]
-        
-        return RockProperties(rho_gp, vp_gp, vs_gp)
+        return RockProperties(rho, vp, vs)
 
     def calc_gas_sand_properties(self, z_rho, z_vp, z_vs):
-        grid = self._load_gas_grid()
+        cheb_params = load_chebyshev_approximation('../data/grids/chebyshev/gas_pl_vp.json')
         
-        # Note: gas grid only has 2 properties (rho, vp)
-        samples = sample_gp_grid(
-            grid,
-            z_new=[z_rho, z_vp],
-            n_samples=1,
-            linear_extrap_funcs=[
-                self.calc_gas_sand_rho,
-                self.calc_gas_sand_vp
-            ]
-        )
+        # Gas grid has only 2 properties: rho and Vp
+        mean_rho, _ = fast_chebyshev_evaluate(cheb_params, z_rho)
+        mean_rho = mean_rho[:, 0]
+        mask_rho = (z_rho >= 1250) & (z_rho <= 3800)
+        rho = self.calc_gas_sand_rho(z_rho)
+        rho[mask_rho] = mean_rho[mask_rho]
+
+        mean_vp, _ = fast_chebyshev_evaluate(cheb_params, z_vp)
+        mean_vp = mean_vp[:, 1]
+        mask_vp = (z_vp >= 1250) & (z_vp <= 3800)
+        vp = self.calc_gas_sand_vp(z_vp)
+        vp[mask_vp] = mean_vp[mask_vp]
+
+        # Vs uses polynomial model only (not in gas grid)
+        vs = self.calc_gas_sand_vs(z_vs)
         
-        rho_gp = samples[0, :len(z_rho), 0]
-        vp_gp = samples[0, :len(z_vp), 1]
-        vs_gp = self.calc_gas_sand_vs(z_vs)  # Use linear model for Vs
-        
-        return RockProperties(rho_gp, vp_gp, vs_gp)
+        return RockProperties(rho, vp, vs)
